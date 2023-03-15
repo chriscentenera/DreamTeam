4b5554be4fdee2311a6bd5b38aa6b3fb632077af
aschfabre <Lawrence_zong@hotmail.com>
2023-03-14 17:16:22 -0700

    Added test JSON data with proper routing

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,26 @@ const express = require('express')
 const app = express()
 const port = 3030
 
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Home page')
 })
+
+app.get('/api/players', (req, res) => {
+
+    const data = {
+        "name": "Elias Pettersson",
+        "position": "Forward",
+        "number": "40"
+    };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.json(data);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
