71ce7cb8ec1599878f50d2ca2c751830b31c1767
Aschfabre <lawrence_zong@hotmail.com>
2023-04-27 17:22:56 -0700

    Added bulma library, PlayerCard component and css styling (#22)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
+import 'bulma/css/bulma.css'
+
 import React, { useState, useEffect } from 'react';
-import { Player } from './components/Player';
+import PlayerCard from './components/PlayerCard';
 
 function getData(endpoint, setState) {
     fetch(endpoint)
@@ -9,24 +11,23 @@ function getData(endpoint, setState) {
 }
 
 function App() {
-    const testplayername = 'Elias Pettersson'.replace(/ /g,'%20');
-    const [player, setPlayer] = useState([]);
     const [allPlayers, setAllPlayers] = useState([]);
-    
-    useEffect(() => getData('http://localhost:3030/players/'+testplayername, setPlayer), [testplayername]);
+
     useEffect(() => getData('http://localhost:3030/players', setAllPlayers), []);
 
     const listItems = allPlayers.map(player =>
-        <Player key={player._id} player={player}/>
+        <div className="column is-one-quarter"> 
+            <PlayerCard key={player._id} playerData={player}/> 
+        </div>
     )
 
     return (
-        <div>
-            <h1>Select Player: {player.name}</h1>
-            <ul><Player player={player}/></ul>
-
-            <h1>All Hockey Players In Endpoint</h1>
-            <ul>{listItems}</ul>
+        <div className="container">
+            <section className="section">
+                <div className="columns is-multiline">
+                    {listItems}
+                </div>
+            </section>
         </div>
     );
 }
